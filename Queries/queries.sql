831243fe9e70ce50dc548cffb14714fe8b7fd3ab
chloebellehooton <77647315+chloebellehooton@users.noreply.github.com>
2021-06-30 20:53:41 -0700

    finishing up pre-challenge work

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -108,3 +108,101 @@ ORDER BY de.dept_no;
 
 -- Check Table
 select * from count_current_emp
+
+
+-- BOSS REQUEST: EMPLOYEE INFORMATIOIN
+-- Make new table with: emp #, last name, first name, gender, to_date, salary
+
+-- check salary table, need more info
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- Pull employment dates from dept_emp, make new table
+-- SELECT emp_no, 
+-- 	first_name, 
+-- 	last_name,
+-- 	gender
+-- INTO emp_info
+-- FROM employees
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- drop table emp_info
+
+-- Join emp_info with salary table to add to_date and salary
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+on e.emp_no = s.emp_no
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+     AND (de.to_date = '1999-01-01');
+	 
+-- BOSS REQUEST: MANAGEMENT
+-- Make new table with: manager's employee number, first name, last name, from_date, to_date
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- BOSS REQUEST: DEPARTMENT RETIREES
+-- Add departments to the current_emp table
+-- emp_no, first_name, last_name, dept_name
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no);
+
+
+-- SALES REQUEST
+-- Create table with columns for sales: emp_no, first_name, last_name, dept_name
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	de.dept_no,
+	d.dept_name
+--INTO sales_info
+FROM current_emp as ce
+	INNER JOIN dept_emp as de
+		on (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales');
+
+-- SALES & DEVELOPMENT REQUEST
+-- Create table with columns for sales & dev: emp_no, first_name, last_name, dept_name
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	de.dept_no,
+	d.dept_name
+--INTO sales_dev_info
+FROM current_emp as ce
+	INNER JOIN dept_emp as de
+		on (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
