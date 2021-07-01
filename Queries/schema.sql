dc52b0d64a022fbff0af0478d40a28b718536676
chloebellehooton <77647315+chloebellehooton@users.noreply.github.com>
2021-06-30 17:10:18 -0700

    updating code

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,4 +1,5 @@
 -- Creating tables for PH-EmployeeDB
+
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -35,16 +36,6 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_emp (
-	dept_no VARCHAR NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no, emp_no)
-);
-
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
@@ -54,4 +45,17 @@ CREATE TABLE titles (
 	PRIMARY KEY (emp_no, title, from_date)
 );
 
+drop table dept_emp
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+
 SELECT * FROM departments;
+
+SELECT * FROM dept_manager;
