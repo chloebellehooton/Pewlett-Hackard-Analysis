8be4b0fb68cebda525d7d7ec47e97107a96b6a9d
chloebellehooton <77647315+chloebellehooton@users.noreply.github.com>
2021-06-30 22:11:15 -0700

    deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -32,3 +32,22 @@ GROUP BY title
 ORDER BY count DESC;
 
 
+-- DELIVERABLE 2: The Employees Eligible for the Mentorship Program
+-- Create mentorship-elegibiliity table for current_emp born between January 1, 1965 and December 31, 1965
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = ('1999-01-01'))
+     AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
