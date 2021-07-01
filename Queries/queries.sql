fc900c70e5f2989600cacd56aa54ec6433df56aa
chloebellehooton <77647315+chloebellehooton@users.noreply.github.com>
2021-06-30 19:05:19 -0700

    updates

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -15,8 +15,96 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Create retirement eligibility table
-SELECT first_name, last_name
+-- SELECT first_name, last_name
+-- INTO retirement_info
+-- FROM employees
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+SELECT first_name, last_name, title
+FROM employees as e
+LEFT JOIN titles as t ON e.emp_no = t.emp_no
+
+-- drop retirement table so can redo 
+DROP TABLE retirement_info; 
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Nickname version of query above
+-- These aliases only exist within this query and aren't committed to that database
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de	
+ON ri.emp_no = de.emp_no;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Going back and redoing dept & dept_manager query with nicknames
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Left Join for retirement_info and dept_emp
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de_emp.to_date = ('1999-01-01');
+
+-- Check table
+select * from retirement_info
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+-- Employee count by department number (ordered) into table
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO count_current_emp
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Check Table
+select * from count_current_emp
